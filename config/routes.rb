672f42a2662963a9d7849b68e76278b5fb878238
NIC NOVELLI <NJ.NOVELLI@GMAIL.OM>
2017-02-14 13:16:57 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'sign_in/index'
+
+  root 'sign_in#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
